37ac260061acd29cc613de66caf1e433d5b63b8f
dandimeo <daniele@arangodb.com>
2023-06-02 14:39:34 +0200

    implement /generate-commit

--- app.js
+++ app.js
@@ -65,16 +65,14 @@ module.exports = (app, { getRouter }) => {
 
     context.log("Comment: " + comment)
 
-    if (comment == "/generate") {
-
+    if (comment.includes("/generate")) {
       const pr_body = context.payload.issue.body;
-      // context.log("PR Body: " + pr_body)
-
       const body_lines = pr_body.match(/[^\r\n]+/g);
 
       var ci_params = await pull_request.parsePRDescription(body_lines, context.octokit);
       ci_params["workflow"] = "generate"
       ci_params["generators"] = "examples api-docs"
+      if (comment.includes("commit")) ci_params["commit-generated"] = true
 
       const branch_info =  await pull_request.getBranchFromPRNumber(context.octokit, "arangodb", "docs-hugo", context.payload.issue.number)
       if (branch_info == undefined || branch_info.branch == undefined) {
@@ -82,7 +80,6 @@ module.exports = (app, { getRouter }) => {
         pull_request.createPRComment(context.octokit, "arangodb", "docs-hugo", context.payload.pull_request.number, "There was an error triggering checks!")
         return
       }
-      // console.log(branch_info)
       
       let pipeline_id = await circleci.triggerCircleCIPipeline(branch_info.branch, ci_params)
       app.log.info("PIPELINE ID " + pipeline_id)
@@ -92,13 +89,6 @@ module.exports = (app, { getRouter }) => {
       }
 
       pull_request.createPRComment(context.octokit, "arangodb", "docs-hugo", context.payload.issue.number, "Checks correctly triggered!")
-
-
-      // let jobs = await circleci.getPipelineJobs(pipeline_id)
-      // for (let job of jobs) {
-      //   // circleci.createJobCheck(job, branch_info)
-      //   // app.log.info("check created")
-      // }
     }
   }
 

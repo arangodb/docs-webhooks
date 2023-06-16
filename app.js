77c844581bafdb8e999d347dd6dfab6aaacace3b
dandimeo <daniele@arangodb.com>
2023-06-16 08:15:23 +0200

    implement commit-generated flow

--- app.js
+++ app.js
@@ -83,7 +83,29 @@ module.exports = (app, { getRouter }) => {
         return
       }
 
-      pull_request.createPRComment(context.octokit, "arangodb", "docs-hugo", context.payload.issue.number, "Checks correctly triggered!")
+    }
+
+    if (comment == "/commit-generated") {
+      const pr_body = context.payload.issue.body;
+      const body_lines = pr_body.match(/[^\r\n]+/g);
+
+      var ci_params = await pull_request.parsePRDescription(body_lines, context.octokit);
+      ci_params["workflow"] = "commit-generated"
+
+      const branch_info =  await pull_request.getBranchFromPRNumber(context.octokit, "arangodb", "docs-hugo", context.payload.issue.number)
+      if (branch_info == undefined || branch_info.branch == undefined) {
+        app.log.info("[ERROR] [pullRequestComment] branch_info undefined")
+        pull_request.createPRComment(context.octokit, "arangodb", "docs-hugo", context.payload.pull_request.number, "There was an error triggering checks!")
+        return
+      }
+      
+      let pipeline_id = await circleci.triggerCircleCIPipeline(branch_info.branch, ci_params)
+      app.log.info("PIPELINE ID " + pipeline_id)
+      if (pipeline_id == undefined) {
+        pull_request.createPRComment(context.octokit, "arangodb", "docs-hugo", context.payload.issue.number, "There was an error triggering checks!")
+        return
+      }
+
     }
   }
 

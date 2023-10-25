2c6744c49867e7e878674fcd839ad10727280f2c
dandimeo <daniele@arangodb.com>
2023-10-25 11:29:45 +0200

    debug log

--- pull_request.js
+++ pull_request.js
@@ -35,7 +35,7 @@ async function parsePRUpstream(line, octokit) {
 
 
 async function getBranchFromPR(octokit, owner, repo, pr) {
-  console.log("[DEBUG] pull_request repo object " + pr.repo )
+  console.log("[DEBUG] pull_request repo object " + pr )
 
   if (pr.repo.full_name != "arangodb/docs-hugo") 
       return {branch: "pull/"+pr.number+"/head", sha: ""};
